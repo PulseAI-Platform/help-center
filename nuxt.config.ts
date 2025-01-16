82d1d994d9d89232890b2736d0abe66aade680f8
Zaela Rose <zaela.rose@proton.me>
2025-01-16 13:26:52 -0600

    Add studio module

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,8 +1,20 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   devtools: { enabled: true },
-  modules: ['@nuxt/content'],
+  modules: ['@nuxt/content', '@nuxthq/studio'],
+  runtimeConfig: {
+    public: {
+      studioApi: import.meta.env.STUDIO_API
+    }
+  },
   routeRules: {
     '/': { prerender: true }
-  }
+  },
+  content: {
+    studio: {
+      enabled: true,
+      dev: true,
+    }
+  },
+  compatibilityDate: '2025-01-15'
 })
