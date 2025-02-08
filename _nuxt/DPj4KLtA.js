709a46cb1bdfdfc29c2d19f323d8b2e0c8d6a200
Matt <matt@goblinit.com>
2025-02-08 11:44:16 -0600

    Updates

---
+++ _nuxt/DPj4KLtA.js
@@ -0,0 +1,1 @@
+import s from"./B4d_x6qa.js";import{a as o,D as u,Q as f,N as m}from"./CcZWyMyr.js";import"./CxYJgpPD.js";const l=o({name:"Markdown",extends:s,setup(t){const{parent:e}=m(),{between:n,default:a}=u(),r=f(()=>typeof t.unwrap=="string"?t.unwrap.split(" "):["*"]);return{fallbackSlot:a,tags:r,between:n,parent:e}}});export{l as default};
